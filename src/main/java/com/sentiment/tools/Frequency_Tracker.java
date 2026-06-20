de576bda5d829101f61ffdbc5161cd2482ee9c7c
Kanmani <kanmanikathiravan1375@gmail.com>
2026-06-20 15:27:17 +0530

    halfway(remove duplicates)

--- src/main/java/com/sentiment/tools/Frequency_Tracker.java
+++ src/main/java/com/sentiment/tools/Frequency_Tracker.java
@@ -1,4 +1,5 @@
 package com.sentiment.tools;
+
 public class Frequency_Tracker {
 	public static void main(String[] args) {
 
@@ -8,19 +9,34 @@ public class Frequency_Tracker {
 
 		// Remove punctuations: "amet,"--> "amet"
 		String p = ". ? ! , ; : — - \' \" ( ) [ ] { }";
+		String no_punctuation_sentence="";
 		for (int i = 0; i < word.length; i++) {
-			String no_punctuations=" ";
-			for(int j=0;j<word[i].length();j++) {
-				if(!p.contains(word[i].substring(j,j+1))) {
-					no_punctuations=no_punctuations+word[i].charAt(j);
+			String no_punctuations = ""+" ";
+			for (int j = 0; j < word[i].length(); j++) {
+				if (!p.contains(word[i].substring(j, j + 1))) {
+					no_punctuations = no_punctuations + word[i].charAt(j);
 				}
 			}
-			String[] no_punctuationsA= no_punctuations.split(" ");
-			System.out.println(no_punctuationsA[1]);
-					
+			no_punctuation_sentence=no_punctuation_sentence+no_punctuations;
 		}
+		System.out.println(no_punctuation_sentence);
+	
+		//removing repeated words
+		String[] no_punctuation_sentenceA= no_punctuation_sentence.split(" ");
+		for(int i=0;i<no_punctuation_sentenceA.length;i++) {
+			String[] no_repeat=new String[100];
+			for(int j=0;j<no_repeat.length;j++) {
+				if(!no_punctuation_sentenceA[i].equals(no_repeat[j])) {
+					//no_repeat[j]=no_punctuation_sentenceA[i];
+				}
+				no_repeat[j]=no_punctuation_sentenceA[i];
 
+				
+			}
+		System.out.println(no_repeat[]);
 
+			
+		}
 
 		// removing repeated elements from the string
 		String remove_dupe = "";
